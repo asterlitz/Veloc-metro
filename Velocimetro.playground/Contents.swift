cc3cc1d6cee4215dfc4221e84a2cb2cdc7eb403b
Daniel Torres Mateo <asterlitz.17@gmail.com>
2016-02-20 14:29:19 -0600

    Playground velocimetro

---
+++ Velocimetro.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+// Autor: Daniel Alberto Torres Mateo
+import UIKit
+
+enum Velocidades:Int{
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init(velocidadInicial:Velocidades){
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad:Velocidades;
+    
+    init(){
+        velocidad = Velocidades(velocidadInicial: Velocidades.Apagado);
+    }
+    
+    func cambioDeVelocidad()->(actual:Int, velocidadEnCadena:String)
+    {
+        var resultado = (actual:self.velocidad.rawValue,velocidadEnCadena:"");
+        
+        switch self.velocidad.rawValue
+        {
+        case Velocidades.Apagado.rawValue:
+            self.velocidad = Velocidades.VelocidadBaja;
+            resultado.velocidadEnCadena = "Apagado";
+        case Velocidades.VelocidadBaja.rawValue:
+            self.velocidad = Velocidades.VelocidadMedia;
+            resultado.velocidadEnCadena = "Velocidad Baja";
+        case Velocidades.VelocidadMedia.rawValue:
+            self.velocidad = Velocidades.VelocidadAlta;
+            resultado.velocidadEnCadena = "Velocidad Media";
+        case Velocidades.VelocidadAlta.rawValue:
+            self.velocidad = Velocidades.VelocidadMedia;
+            resultado.velocidadEnCadena = "Velocidad Alta";
+        default:
+            break;
+        }
+        return resultado;
+    }
+}
+
+//Prueba
+var auto = Auto();
+for i in 1...20
+{
+    var tupla = auto.cambioDeVelocidad();
+    print( "\(i). \(tupla.actual) "+tupla.velocidadEnCadena);
+}
